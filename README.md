939a5473827a6f4cbf0a16cb4fbb891f21b1c69b
gasvn <nanoc@live.cn>
2019-07-04 11:36:33 +0800

    commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Res2Net
+The official pytorch implemention of the paper ["Res2Net: A New Multi-scale Backbone Architecture"](https://arxiv.org/pdf/1904.01169.pdf)
+## Introduction
+We propose a novel building block for CNNs, namely Res2Net, by constructing hierarchical residual-like
+connections within one single residual block. The Res2Net represents multi-scale features at a granular level and increases the range
+of receptive fields for each network layer. The proposed Res2Net block can be plugged into the state-of-the-art backbone CNN models,
+e.g. , ResNet, ResNeXt, BigLittleNet, and DLA. We evaluate the Res2Net block on all these models and demonstrate consistent performance gains over baseline models.
+<p align="center">
+	<img src="figures/res2net_structure.png" alt="Sample"  width="500">
+	<p align="center">
+		<em>Res2Net module</em>
+	</p>
+</p>
+
+## Useage
+### Requirement
+PyTorch>=0.4.1 (PyTorch1.0 and 1.1 are supported.)
+### Examples 
+```
+git clone https://github.com/gasvn/Res2Net.git
+
+from res2net import res2net50
+model = res2net50(pretrained=True)
+```
+(By default, the model will be downloaded automatically.
+If the default download link is not available, please refer to the Download Link listed on **Pretrained models**.)
+## Pretrained models
+| model |#Params | GFLOPs |top-1 error| top-5 error| Link |
+| :--: | :--: | :--: | :--: | :--: | :--: |
+| Res2Net-50-48w-2s  | 25.29M | 4.2 | 22.68 | 6.47 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPbo7RnRUz-7ejhLg?e=gU2EZG)
+| Res2Net-50-26w-4s  | 25.70M | 4.2 | 22.01 | 6.15 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPbMavn7eawKhvCPY?e=TBHOuT)
+| Res2Net-50-14w-8s  | 25.06M | 4.2 | 21.86 | 6.14 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPdOTqhF8ne_aakDI?e=EVb8Ri)
+| Res2Net-50-26w-6s  | 37.05M | 6.3 | 21.42 | 5.87 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPc2mqy1h8324sxxI?e=Go4p7I)
+| Res2Net-50-26w-8s  | 48.40M | 8.3 | 20.80 | 5.63 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPdTrAd_Afzc26Z7Q?e=slYqsR)
+| Res2Net-101-26w-4s | 45.21M | 8.1 | 20.81 | 5.57 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPcJRgTLkahL0cFYw?e=nwbnic)
+| Res2NeXt-50        | 24.67M | 4.2 | 21.76 | 6.09 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPcWlWLXBuKxma7DQ?e=mt4dQf)
+| Res2Net-DLA-60     | 21.15M | 4.2 | 21.53 | 5.80 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPbWAqdcatece24vs?e=t3shXH)
+| Res2NeXt-DLA-60    | 17.33M | 3.6 | 21.55 | 5.86 |[OneDrive](https://1drv.ms/u/s!AkxDDnOtroRPcjxCM0kAYHEaEd0?e=9WrBpj)
+## Applications
+- Instance segmentation (Coming soon)
+- Segmantic segmentation (Coming soon)
+- More applications will be released soon
+
+## Citation
+If you find this work or code is helpful in your research, please cite:
+```
+@article{gao2019res2net,
+  title={Res2Net: A New Multi-scale Backbone Architecture},
+  author={Gao, Shang-Hua and Cheng, Ming-Ming and Zhao, Kai and Zhang, Xin-Yu and Yang, Ming-Hsuan and Torr, Philip},
+  journal={arXiv preprint arXiv:1904.01169},
+  year={2019}
+}
+```
---
+++ dla.py
@@ -0,0 +1,397 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import math
+from os.path import join
+
+import torch
+from torch import nn
+import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+
+BatchNorm = nn.BatchNorm2d
+
+__all__ = ['res2net_dla60']
+
+
+model_urls = {
+    'res2net_dla60': 'http://mc.nankai.edu.cn/projects/res2net/pretrainmodels/res2net_dla60_4s-d88db7f9.pth',
+    'res2next_dla60': 'http://mc.nankai.edu.cn/projects/res2net/pretrainmodels/res2next_dla60_4s-d327927b.pth',
+
+}
+
+
+
+
+class Bottle2neck(nn.Module):
+    """
+    RexNeXt bottleneck type C
+    """
+    expansion = 2
+
+    def __init__(self, inplanes, planes, stride=1, dilation=1, baseWidth=28, scale = 4):
+        """ Constructor
+        Args:
+            inplanes: input channel dimensionality
+            planes: output channel dimensionality
+            stride: conv stride. Replaces pooling layer.
+            downsample: None when stride = 1
+            baseWidth: basic width of conv3x3
+            scale: number of scale.
+            type: 'normal': normal set. 'stage': frist blokc of a new stage.
+        """
+        super(Bottle2neck, self).__init__()
+        if stride != 1:
+            stype = 'stage'
+        else:
+            stype = 'normal'
+        width = int(math.floor(planes * (baseWidth/128.0)))
+        self.conv1 = nn.Conv2d(inplanes, width*scale, kernel_size=1, bias=False)
+        self.bn1 = BatchNorm(width*scale)
+        
+        if scale == 1:
+          self.nums = 1
+        else:
+          self.nums = scale -1
+        if stype == 'stage':
+            self.pool = nn.AvgPool2d(kernel_size=3, stride = stride, padding=1)
+        convs = []
+        bns = []
+        for i in range(self.nums):
+          convs.append(nn.Conv2d(width, width, kernel_size=3, stride = stride, 
+                        padding=dilation, dilation=dilation, bias=False))
+          bns.append(BatchNorm(width))
+        self.convs = nn.ModuleList(convs)
+        self.bns = nn.ModuleList(bns)
+
+        self.conv3 = nn.Conv2d(width*scale, planes, kernel_size=1, bias=False)
+        self.bn3 = BatchNorm(planes)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.stype = stype
+        self.scale = scale
+        self.width  = width
+
+    def forward(self, x, residual=None):
+        if residual is None:
+            residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        spx = torch.split(out, self.width, 1)
+        for i in range(self.nums):
+          if i==0 or self.stype=='stage':
+            sp = spx[i]
+          else:
+            sp = sp + spx[i]
+          sp = self.convs[i](sp)
+          sp = self.relu(self.bns[i](sp))
+          if i==0:
+            out = sp
+          else:
+            out = torch.cat((out, sp), 1)
+        if self.scale != 1 and self.stype=='normal':
+          out = torch.cat((out, spx[self.nums]),1)
+        elif self.scale != 1 and self.stype=='stage':
+          out = torch.cat((out, self.pool(spx[self.nums])),1)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottle2neckX(nn.Module):
+    """
+    RexNeXt bottleneck type C
+    """
+    expansion = 2
+    cardinality = 8
+    def __init__(self, inplanes, planes, stride=1, dilation=1, scale = 4):
+        """ Constructor
+        Args:
+            inplanes: input channel dimensionality
+            planes: output channel dimensionality
+            stride: conv stride. Replaces pooling layer.
+            downsample: None when stride = 1
+            baseWidth: basic width of conv3x3
+            scale: number of scale.
+            type: 'normal': normal set. 'stage': frist blokc of a new stage.
+        """
+        super(Bottle2neckX, self).__init__()
+        if stride != 1:
+            stype = 'stage'
+        else:
+            stype = 'normal'
+        cardinality =  Bottle2neckX.cardinality
+        width = bottle_planes = planes * cardinality // 32
+        self.conv1 = nn.Conv2d(inplanes, width*scale, kernel_size=1, bias=False)
+        self.bn1 = BatchNorm(width*scale)
+        
+        if scale == 1:
+          self.nums = 1
+        else:
+          self.nums = scale -1
+        if stype == 'stage':
+            self.pool = nn.AvgPool2d(kernel_size=3, stride = stride, padding=1)
+        convs = []
+        bns = []
+        for i in range(self.nums):
+          convs.append(nn.Conv2d(width, width, kernel_size=3, stride = stride, 
+                        padding=dilation, dilation=dilation, groups=cardinality, bias=False))
+          bns.append(BatchNorm(width))
+        self.convs = nn.ModuleList(convs)
+        self.bns = nn.ModuleList(bns)
+
+        self.conv3 = nn.Conv2d(width*scale, planes, kernel_size=1, bias=False)
+        self.bn3 = BatchNorm(planes)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.stype = stype
+        self.scale = scale
+        self.width  = width
+
+    def forward(self, x, residual=None):
+        if residual is None:
+            residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        spx = torch.split(out, self.width, 1)
+        for i in range(self.nums):
+          if i==0 or self.stype=='stage':
+            sp = spx[i]
+          else:
+            sp = sp + spx[i]
+          sp = self.convs[i](sp)
+          sp = self.relu(self.bns[i](sp))
+          if i==0:
+            out = sp
+          else:
+            out = torch.cat((out, sp), 1)
+        if self.scale != 1 and self.stype=='normal':
+          out = torch.cat((out, spx[self.nums]),1)
+        elif self.scale != 1 and self.stype=='stage':
+          out = torch.cat((out, self.pool(spx[self.nums])),1)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+
+class Root(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, residual):
+        super(Root, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, 1,
+            stride=1, bias=False, padding=(kernel_size - 1) // 2)
+        self.bn = BatchNorm(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.residual = residual
+
+    def forward(self, *x):
+        children = x
+        x = self.conv(torch.cat(x, 1))
+        x = self.bn(x)
+        if self.residual:
+            x += children[0]
+        x = self.relu(x)
+
+        return x
+
+
+class Tree(nn.Module):
+    def __init__(self, levels, block, in_channels, out_channels, stride=1,
+                 level_root=False, root_dim=0, root_kernel_size=1,
+                 dilation=1, root_residual=False):
+        super(Tree, self).__init__()
+        if root_dim == 0:
+            root_dim = 2 * out_channels
+        if level_root:
+            root_dim += in_channels
+        if levels == 1:
+            self.tree1 = block(in_channels, out_channels, stride,
+                               dilation=dilation)
+            self.tree2 = block(out_channels, out_channels, 1,
+                               dilation=dilation)
+        else:
+            self.tree1 = Tree(levels - 1, block, in_channels, out_channels,
+                              stride, root_dim=0,
+                              root_kernel_size=root_kernel_size,
+                              dilation=dilation, root_residual=root_residual)
+            self.tree2 = Tree(levels - 1, block, out_channels, out_channels,
+                              root_dim=root_dim + out_channels,
+                              root_kernel_size=root_kernel_size,
+                              dilation=dilation, root_residual=root_residual)
+        if levels == 1:
+            self.root = Root(root_dim, out_channels, root_kernel_size,
+                             root_residual)
+        self.level_root = level_root
+        self.root_dim = root_dim
+        self.downsample = None
+        self.project = None
+        self.levels = levels
+        if stride > 1:
+            self.downsample = nn.MaxPool2d(stride, stride=stride)
+        if in_channels != out_channels:
+            self.project = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels,
+                          kernel_size=1, stride=1, bias=False),
+                BatchNorm(out_channels)
+            )
+
+    def forward(self, x, residual=None, children=None):
+        children = [] if children is None else children
+        bottom = self.downsample(x) if self.downsample else x
+        residual = self.project(bottom) if self.project else bottom
+        if self.level_root:
+            children.append(bottom)
+        x1 = self.tree1(x, residual)
+        if self.levels == 1:
+            x2 = self.tree2(x1)
+            x = self.root(x2, x1, *children)
+        else:
+            children.append(x1)
+            x = self.tree2(x1, children=children)
+        return x
+
+
+class DLA(nn.Module):
+    def __init__(self, levels, channels, num_classes=1000,
+                 block=Bottle2neck, residual_root=False, return_levels=False,
+                 pool_size=7, linear_root=False):
+        super(DLA, self).__init__()
+        self.channels = channels
+        self.return_levels = return_levels
+        self.num_classes = num_classes
+        self.base_layer = nn.Sequential(
+            nn.Conv2d(3, channels[0], kernel_size=7, stride=1,
+                      padding=3, bias=False),
+            BatchNorm(channels[0]),
+            nn.ReLU(inplace=True))
+        self.level0 = self._make_conv_level(
+            channels[0], channels[0], levels[0])
+        self.level1 = self._make_conv_level(
+            channels[0], channels[1], levels[1], stride=2)
+        self.level2 = Tree(levels[2], block, channels[1], channels[2], 2,
+                           level_root=False,
+                           root_residual=residual_root)
+        self.level3 = Tree(levels[3], block, channels[2], channels[3], 2,
+                           level_root=True, root_residual=residual_root)
+        self.level4 = Tree(levels[4], block, channels[3], channels[4], 2,
+                           level_root=True, root_residual=residual_root)
+        self.level5 = Tree(levels[5], block, channels[4], channels[5], 2,
+                           level_root=True, root_residual=residual_root)
+
+        self.avgpool = nn.AvgPool2d(pool_size)
+        self.fc = nn.Conv2d(channels[-1], num_classes, kernel_size=1,
+                            stride=1, padding=0, bias=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, BatchNorm):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_level(self, block, inplanes, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or inplanes != planes:
+            downsample = nn.Sequential(
+                nn.MaxPool2d(stride, stride=stride),
+                nn.Conv2d(inplanes, planes,
+                          kernel_size=1, stride=1, bias=False),
+                BatchNorm(planes),
+            )
+
+        layers = []
+        layers.append(block(inplanes, planes, stride, downsample=downsample))
+        for i in range(1, blocks):
+            layers.append(block(inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def _make_conv_level(self, inplanes, planes, convs, stride=1, dilation=1):
+        modules = []
+        for i in range(convs):
+            modules.extend([
+                nn.Conv2d(inplanes, planes, kernel_size=3,
+                          stride=stride if i == 0 else 1,
+                          padding=dilation, bias=False, dilation=dilation),
+                BatchNorm(planes),
+                nn.ReLU(inplace=True)])
+            inplanes = planes
+        return nn.Sequential(*modules)
+
+    def forward(self, x):
+        y = []
+        x = self.base_layer(x)
+        for i in range(6):
+            x = getattr(self, 'level{}'.format(i))(x)
+            y.append(x)
+        if self.return_levels:
+            return y
+        else:
+            x = self.avgpool(x)
+            x = self.fc(x)
+            x = x.view(x.size(0), -1)
+
+            return x
+
+    def load_pretrained_model(self, data_name, name):
+        assert data_name in dataset.__dict__, \
+            'No pretrained model for {}'.format(data_name)
+        data = dataset.__dict__[data_name]
+        fc = self.fc
+        if self.num_classes != data.classes:
+            self.fc = nn.Conv2d(
+                self.channels[-1], data.classes,
+                kernel_size=1, stride=1, padding=0, bias=True)
+        try:
+            model_url = get_model_url(data, name)
+        except KeyError:
+            raise ValueError(
+                '{} trained on {} does not exist.'.format(data.name, name))
+        self.load_state_dict(model_zoo.load_url(model_url))
+        self.fc = fc
+
+
+
+
+def res2net_dla60(pretrained=None, **kwargs):
+    Bottle2neck.expansion = 2
+    model = DLA([1, 1, 1, 2, 3, 1],
+                [16, 32, 128, 256, 512, 1024],
+                block=Bottle2neck, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['res2net_dla60']))
+    return model
+
+def res2next_dla60(pretrained=None, **kwargs):
+    Bottle2neckX.expansion = 2
+    model = DLA([1, 1, 1, 2, 3, 1],
+                [16, 32, 128, 256, 512, 1024],
+                block=Bottle2neckX, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['res2next_dla60']))
+    return model
+
+
+if __name__ == '__main__':
+    images = torch.rand(1, 3, 224, 224).cuda(0)
+    model = res2next_dla60(pretrained=True)
+    model = model.cuda(0)
+    print(model(images).size())
